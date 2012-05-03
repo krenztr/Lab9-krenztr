6265d56db8324b1eb1d98f675cec3166fd88c992
Trevor Krenz <krenztr@rose-hulman.edu>
2012-05-03 10:54:57 -0400

    Game listing implementation

--- ChatBot/chatbot.rb
+++ ChatBot/chatbot.rb
@@ -2,3 +2,8 @@ STDOUT.sync = true
 puts 'What is your name?'
 name = gets.chomp
 puts 'Hello, '+name+'!'
+puts '1. Global Thermonuclear War'
+puts '2. Solitare'
+puts '3. Minesweeper'
+puts '4. Chess'
+puts '5. Pinball'
