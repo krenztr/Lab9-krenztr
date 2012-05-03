cfb94f424d3114cd457bb0640a1c77e2354cc5fa
Trevor Krenz <krenztr@rose-hulman.edu>
2012-05-03 10:42:35 -0400

    Task 4

--- ChatBot/features/step_definitions/cli_steps.rb
+++ ChatBot/features/step_definitions/cli_steps.rb
@@ -2,6 +2,10 @@ Given /^the application is running$/ do
   run_interactive(unescape("ruby chatbot.rb"))
 end
 
+Given /^I typed in something$/ do
+  type("Test")
+end
+
 Then /^I should see "([^""]*)"$/ do |text|
   assert_partial_output(text, all_output)
 end
